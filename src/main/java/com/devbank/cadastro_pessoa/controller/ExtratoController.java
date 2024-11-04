a17e6015cef4896a223aa0a947df56a9dd5dd94c
Pedro Munhoz <pedro.munhoz.pessoal@gmail.com>
2024-11-04 01:31:04 +0000

    feature: consulta saldo

--- src/main/java/com/devbank/cadastro_pessoa/controller/ExtratoController.java
+++ src/main/java/com/devbank/cadastro_pessoa/controller/ExtratoController.java
@@ -1,6 +1,7 @@
 package com.devbank.cadastro_pessoa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,24 +28,48 @@ public class ExtratoController {
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    @GetMapping
+    // Método para obter o extrato dos últimos 30 dias
+    @GetMapping("/detalhado")
     public ResponseEntity<List<HistoricoMovimentacoes>> obterExtratoUltimos30Dias(@RequestParam Integer idPessoa) {
         // Verifica se a pessoa existe no banco
         Optional<Pessoa> optionalPessoa = pessoaRepository.findById(idPessoa);
         
         if (optionalPessoa.isEmpty()) {
-            return ResponseEntity.status(404).body(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         Pessoa pessoa = optionalPessoa.get();
 
         // Verifica se o saldo associado à pessoa existe
         Optional<Saldo> optionalSaldo = saldoRepository.findByPessoa(pessoa);
         if (optionalSaldo.isEmpty()) {
-            return ResponseEntity.status(404).body(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
         Saldo saldo = optionalSaldo.get();
         List<HistoricoMovimentacoes> extrato = extratoService.obterExtratoUltimos30Dias(saldo);
         return ResponseEntity.ok(extrato);
+    }
+
+    // Método para obter o saldo pelo ID da pessoa
+    @GetMapping("/saldo")
+    public ResponseEntity<Object> obterSaldoPorIdPessoa(@RequestParam Integer idPessoa) {
+        // Buscar a pessoa pelo ID
+        Optional<Pessoa> optionalPessoa = pessoaRepository.findById(idPessoa);
+
+        if (optionalPessoa.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+        }
+
+        Pessoa pessoa = optionalPessoa.get();
+
+        // Buscar o saldo pela pessoa
+        Optional<Saldo> optionalSaldo = saldoRepository.findByPessoa(pessoa);
+
+        if (optionalSaldo.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Saldo não encontrado para a pessoa fornecida");
+        }
+
+        Saldo saldo = optionalSaldo.get();
+        return ResponseEntity.ok(saldo);
     }
 }
--- src/main/java/com/devbank/cadastro_pessoa/controller/PessoaController.java
+++ src/main/java/com/devbank/cadastro_pessoa/controller/PessoaController.java
@@ -5,7 +5,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,50 +23,7 @@ public class PessoaController {
     @PostMapping("/cadastrar")
     public ResponseEntity<String> cadastrarPessoa(@RequestBody @Valid PessoaCadastroDTO pssoaCadastroDTO ) {
         try {
-
-            // // Simulação de teste interno
-            // PessoaCadastroDTO mockPessoaCadastroDTO = new PessoaCadastroDTO();
-            // mockPessoaCadastroDTO.setPessoa(new PessoaDTO());
-            // mockPessoaCadastroDTO.getPessoa().setTipo("FISICA");
-            // mockPessoaCadastroDTO.getPessoa().setTelefone("1438584242");
-            // mockPessoaCadastroDTO.getPessoa().setEmail("pedro2.silva@example.com");
-
-            // PessoaFisicaDTO pessoaFisicaDTO = new PessoaFisicaDTO();
-            // pessoaFisicaDTO.setNome("Pedro Munhoz");
-            // pessoaFisicaDTO.setIdade(30);
-            // pessoaFisicaDTO.setCpf("145154213");
-
-            // mockPessoaCadastroDTO.setPessoaFisicaDTO(pessoaFisicaDTO);
-
-            // // PessoaJuridicaDTO pessoaJuridicaDTO = new PessoaJuridicaDTO();
-            // // pessoaJuridicaDTO.setNomeFantasia("Empresa XYZ");
-            // // pessoaJuridicaDTO.setCnpj("12345678000199");
-            // // pessoaJuridicaDTO.setRazaoSocial("Empresa XYZ Ltda");
-            // // pessoaJuridicaDTO.setEmail("contato@empresa.xyz");
-            // // mockPessoaCadastroDTO.setPessoaJuridicaDTO(pessoaJuridicaDTO);
-
-            // LoginDTO loginDTO = new LoginDTO();
-            // loginDTO.setEmail("pedro2.silva@example.com");
-            // loginDTO.setSenha("senhaSegura123");
-            // mockPessoaCadastroDTO.setLogin(loginDTO);
-
-            // EnderecoDTO enderecoDTO = new EnderecoDTO();
-            // enderecoDTO.setLogradouro("Rua das Flores");
-            // enderecoDTO.setNumero("123");
-            // enderecoDTO.setBairro("Centro");
-            // enderecoDTO.setCidade("São Paulo");
-            // enderecoDTO.setEstado("SP");
-            // enderecoDTO.setCep("12345678");
-            // mockPessoaCadastroDTO.setEndereco(enderecoDTO);
-
-            // SaldoDTO saldoDTO = new SaldoDTO();
-            // saldoDTO.setSaldoAtual(new BigDecimal("1000.00"));
-            // saldoDTO.setLimiteCredito(new BigDecimal("500.00"));
-            // saldoDTO.setDataUltimaAtualizacao(LocalDate.now());
-            // mockPessoaCadastroDTO.setSaldo(saldoDTO);
-
             cadastroPessoaService.cadastrarPessoa(pssoaCadastroDTO);
-
             return ResponseEntity.status(HttpStatus.CREATED).body("Pessoa cadastrada com sucesso!");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
